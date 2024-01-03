cd7cbfc032999ab46cee8bff4c443ffbc4d18a59
albandrp <96991673+albandrp@users.noreply.github.com>
2024-01-03 22:13:17 +0100

    Update StreamlitApp.py

--- StreamlitApp.py
+++ StreamlitApp.py
@@ -1,24 +1,22 @@
-
 import cv2
 import numpy as np
 import av
-from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration, VideoProcessorBase
 from Image import *
 
 
 
 def process(img):
-    #img.flags.writeable = False
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img_model = Image.get_test()
     img = Image.morphing(img_model,img)
-    return img #cv2.flip(img, 1)
+    return img 
 
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
 
-class VideoProcessor:
+class VideoProcessor(VideoProcessorBase):
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
         img = process(img)
@@ -29,6 +27,6 @@ webrtc_ctx = webrtc_streamer(
     mode=WebRtcMode.SENDRECV,
     rtc_configuration=RTCConfiguration({"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}),
     media_stream_constraints={"video": True, "audio": False},
-    video_processor_factory=VideoProcessor,
+    video_transformer_factory=VideoProcessor,
     async_processing=True,
 )
